234015bd2918d4b83667844d2f34818f5b3ba497
Victor Stinner <vstinner@python.org>
2023-11-21 12:10:51 +0100

    Fix compiler warning in DeadPyUnicode_GetMax()

--- src/unicode.c
+++ src/unicode.c
@@ -9,7 +9,7 @@ DeadPy_UNICODE
 DeadPyUnicode_GetMax(void)
 {
 #if PY_VERSION_HEX >= 0x030A0000
-    return 0x10ffff;
+    return (DeadPy_UNICODE)0x10ffff;
 #else
     return PyUnicode_GetMax();
 #endif
